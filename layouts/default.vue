9392a1f6545f2df28fdcb214265021e31418504f
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-11 00:17:19 +0800

    feat: Added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dark:bg-gray-800">
+    <Header />
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
