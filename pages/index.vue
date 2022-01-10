24bedba90e88938c00a4c507ce6287882481bf7f
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-11 00:19:28 +0800

    remove: Tutorial files

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <Tutorial />
+  <div></div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({
-  name: 'IndexPage',
-})
+export default Vue.extend({})
 </script>
