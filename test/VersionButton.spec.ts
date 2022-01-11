b0cc70d152973344ed89f149ce35cccc13f21a1d
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-11 23:15:18 +0800

    test: Added version button tests

---
+++ test/VersionButton.spec.ts
@@ -0,0 +1,34 @@
+import { shallowMount, RouterLinkStub, Wrapper } from '@vue/test-utils'
+import VersionButton from '~/components/VersionButton.vue'
+
+describe('Version button component', () => {
+  let wrapper: Wrapper<VersionButton>
+
+  beforeAll(() => {
+    jest.spyOn(window, 'addEventListener')
+  })
+
+  beforeEach(() => {
+    wrapper = shallowMount(VersionButton, {
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+  })
+
+  test('is a Vue instance', () => {
+    expect(wrapper.vm).toBeTruthy()
+  })
+
+  it('calls addEventListener on mount', () => {
+    expect(window.addEventListener).toHaveBeenCalled()
+  })
+
+  it('should set the isPopoverOpen flag to true when button is clicked', () => {
+    expect(wrapper.vm.$data.isPopoverOpen).toBe(false)
+
+    wrapper.find('button').trigger('click')
+
+    expect(wrapper.vm.$data.isPopoverOpen).toBe(true)
+  })
+})
