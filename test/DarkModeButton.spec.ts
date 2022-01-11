2b77d7da2e2c5970167a9d4c66daa970b7f2971d
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-11 09:13:24 +0800

    test: Fix tests

--- test/DarkModeButton.spec.ts
+++ test/DarkModeButton.spec.ts
@@ -1,9 +1,25 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import DarkModeButton from '../components/DarkModeButton.vue'
 
 describe('Dark mode button', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    })
+  })
+
   test('is a Vue instance', () => {
-    const wrapper = mount(DarkModeButton)
+    const wrapper = shallowMount(DarkModeButton)
     expect(wrapper.vm).toBeTruthy()
   })
 
--- test/Header.spec.ts
+++ test/Header.spec.ts
@@ -1,9 +1,17 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import Header from '~/components/Header.vue'
+import DarkModeButton from '../components/DarkModeButton.vue'
 
 describe('Header component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Header)
+    const wrapper = shallowMount(Header, {
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+      components: {
+        DarkModeButton,
+      },
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
