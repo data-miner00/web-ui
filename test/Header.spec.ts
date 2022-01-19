da8724677fdae0a5d57c40ce84b365136d338626
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-19 21:52:47 +0800

    test: Supress warning unknow component

--- test/Header.spec.ts
+++ test/Header.spec.ts
@@ -2,6 +2,7 @@ import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import Header from '~/components/Header.vue'
 import DarkModeButton from '../components/DarkModeButton.vue'
 import VersionButton from '~/components/VersionButton.vue'
+import BannerLink from '~/components/BannerLink.vue'
 
 describe('Header component', () => {
   test('is a Vue instance', () => {
@@ -12,6 +13,7 @@ describe('Header component', () => {
       components: {
         DarkModeButton,
         VersionButton,
+        BannerLink,
       },
     })
     expect(wrapper.vm).toBeTruthy()
