2d13050f1124334913fa2c5fb73ccab0c6bf7e22
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-25 00:43:15 +0800

    feat: Working i18n

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div
     class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16 dark:text-white"
   >
-    <header>halo</header>
+    <header>{{ $t('hello') }}</header>
     <div></div>
     <footer></footer>
     <Quicklinks :quicklinks="quicklinks" />
