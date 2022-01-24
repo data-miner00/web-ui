91e9d2873bf0ace752da6531f2431b3beb71c0c4
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-24 23:46:38 +0800

    wip: Added sample quicklink component

---
+++ components/Quicklinks.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="toc fixed z-20 py-10 px-8 overflow-y-auto hidden xl:block">
+    <h5 class="font-bold text-sm mb-4 leading-6">Contents covered</h5>
+    <ul class="leading-6 text-sm">
+      <li v-for="link in quicklinks" :key="link.id">
+        <nuxt-link
+          :to="`#${link.id}`"
+          class="block py-1 font-semibold dark:text-gray-500 dark:hover:text-gray-200"
+        >
+          <span
+            :class="[
+              { 'text-purple-600 font-bold': false },
+              { 'py-2': link.depth === 2, 'ml-2 pb-2': link.depth === 3 },
+            ]"
+            >{{ link.text }}</span
+          >
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    quicklinks: Array,
+  },
+})
+</script>
+
+<style lang="postcss" scoped>
+.toc {
+  width: 19.5rem;
+  top: 4rem;
+  right: max(0px, calc(50% - 45rem));
+  @apply bottom-0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <div
+    class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16 dark:text-white"
+  >
+    <header>halo</header>
+    <div></div>
+    <footer></footer>
+    <Quicklinks :quicklinks="quicklinks" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => ({
+    quicklinks: [
+      {
+        id: '123',
+        depth: 2,
+        text: 'hhhaha',
+      },
+      {
+        id: '1223',
+        depth: 2,
+        text: 'sahaha',
+      },
+      {
+        id: '1423',
+        depth: 2,
+        text: 'hsaaha',
+      },
+      {
+        id: '1623',
+        depth: 2,
+        text: 'hafha',
+      },
+    ],
+  }),
+})
 </script>
