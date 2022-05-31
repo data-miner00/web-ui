f8c77c6c4bd313b579b2f99683d7736151bfd4bc
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-31 22:14:03 +0800

    wip: Added barebone 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-64 xl:pr-16 dark:text-white"
+  >
+    <div v-if="error.statusCode === 404">
+      <h1>Page not found</h1>
+    </div>
+    <div v-else>
+      <h1>An error occurred</h1>
+    </div>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: ['error'],
+  layout: 'default',
+})
+</script>
