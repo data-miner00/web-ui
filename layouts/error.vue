a0d0374cf4152f3d028f544113ca862a46f0dac1
data-miner00 <data-miner00@users.noreply.github.com>
2022-06-05 19:07:21 +0800

    feat: Better 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,21 +2,38 @@
   <div
     class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-64 xl:pr-16 dark:text-white"
   >
-    <div v-if="error.statusCode === 404">
-      <h1>Page not found</h1>
+    <div class="">
+      <div class="text-center py-6">
+        <span class="text-9xl fontbold font-mono">404</span>
+      </div>
+      <h1 class="text-center my-6">
+        {{ errorMessage }}
+      </h1>
     </div>
-    <div v-else>
-      <h1>An error occurred</h1>
+
+    <div class="flex justify-center">
+      <NuxtLink to="/" class="px-4 py-2 bg-pink-400 rounded block mx-auto"
+        >Take me back</NuxtLink
+      >
     </div>
-    <NuxtLink to="/">Home page</NuxtLink>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
   props: ['error'],
   layout: 'default',
+  computed: {
+    errorMessage(): string {
+      switch (this.error.statusCode) {
+        case 404:
+          return 'Regrettably, the destination that you are heading to does not exist.'
+        default:
+          return "Something doesn't went right. Please refresh to see if it helps."
+      }
+    },
+  },
 })
 </script>
