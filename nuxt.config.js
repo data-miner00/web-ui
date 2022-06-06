3afac2c148a767f69e20f3cc1713f31b6f58b770
data-miner00 <data-miner00@users.noreply.github.com>
2022-06-06 16:35:46 +0800

    chore: Update name of project

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'web-ui',
-    titleTemplate: '%s - web-ui',
+    title: 'Gallerium',
+    titleTemplate: '%s - Gallerium',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
