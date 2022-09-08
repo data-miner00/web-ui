b407ed592d2cc9e951b44522758b74092d52a735
data-miner00 <data-miner00@users.noreply.github.com>
2022-09-08 22:36:19 +0800

    chore: Added comments to link models

--- models/links.ts
+++ models/links.ts
@@ -1,12 +1,22 @@
+/*
+ * The model for the links that appears on the sidebar.
+ * Currently used for subcategory link items.
+ */
 export type Link = {
   title: string
   url: string
 }
 
+/*
+ * The link for the sidebar at the very top section.
+ */
 export type MainLink = Link & {
   icon?: string
 }
 
+/*
+ * The model that groups the sublink into a logical section.
+ */
 export type Category = {
   section: string
   basepath: string
